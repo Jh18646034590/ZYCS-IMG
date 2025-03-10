1cd1ca37dfc746fcd6cfef836043b02fbdbc8b15
jh <2106234278@qq.com>
2025-03-10 14:24:23 +0800

    fork后自己项目的修改 更改名称

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <Header title="骤雨重山图床" desc="拒绝流量劫持，全面使用HTTPS" />
+  <Header title="鸿图嘉运图床" desc="拒绝流量劫持，全面使用HTTPS" />
   <main><RouterView /></main>
   <Footer />
   <Toaster />
@@ -9,5 +9,6 @@ import Header from '@/components/Header/Header.vue';
 import Footer from '@/components/Footer/Footer.vue';
 import { Toaster } from '@/components/ui/toast';
 </script>
+
 
 <style scoped></style>
